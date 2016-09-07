abea7ad6b421cfcc66d6dd95457af4b071dd1ee2
mario <morawiec@vp.pl>
2016-09-07 10:58:59 +0200

    start projektu - pliki strony up do 2.3.6

--- menu.php
+++ menu.php
@@ -1,130 +1,199 @@
-<body class="hold-transition skin-blue sidebar-mini">
-<div class="wrapper">
-
-    <header class="main-header">
-        <!-- Logo -->
-        <a href="index.php" class="logo">
-            <!-- mini logo for sidebar mini 50x50 pixels -->
-            <span class="logo-mini"><b>S</b>2.0</span>
-            <!-- logo for regular state and mobile devices -->
-            <span class="logo-lg"><b>Serwis</b>2.0</span>
-        </a>
-<?php include 'pasek.php'; ?>
-    </header>
-    <!-- Left side column. contains the logo and sidebar -->
-    <aside class="main-sidebar">
-        <!-- sidebar: style can be found in sidebar.less -->
-        <section class="sidebar">
-            <!-- Sidebar user panel -->
-            <div class="user-panel">
-                <div class="pull-left image">
-                    <img src="dist/img/programista.png" class="img-circle" alt="User Image">
-                </div>
-                <div class="pull-left info">
-                    <p><?php
-
-
-                        echo"$użytkownik_imie_nazwisko";  ?></p>
-                    <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
-                </div>
+<aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+        <!-- Sidebar user panel -->
+        <div class="user-panel">
+            <div class="pull-left image">
+                <img src="./dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
             </div>
-            <!-- search form -->
-            <form action="index.php?a=wyszukaj" method="post" class="sidebar-form">
-                <div class="input-group">
-                    <input type="text" name="wyszukiwarka" class="form-control" placeholder="Wyszukaj...">
-              <span class="input-group-btn">
-                <button type="submit" id="search-btn" class="btn btn-flat" name="numer" value="Numer"><i class="fa fa-search"></i></button>
+            <div class="pull-left info">
+                <p>Alexander Pierce</p>
+                <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+            </div>
+        </div>
+        <!-- search form -->
+        <form action="#" method="get" class="sidebar-form">
+            <div class="input-group">
+                <input type="text" name="q" class="form-control" placeholder="Search...">
+                <span class="input-group-btn">
+                <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
+                </button>
               </span>
-                </div>
-            </form>
-            <!-- /.search form -->
-            <!-- sidebar menu: : style can be found in sidebar.less -->
-            <ul class="sidebar-menu">
-                <li class="header">MENU</li>
-                <li class="treeview">
-                    <a href="#">
-                        <i class="fa fa-tasks"></i>
-                        <span>Protokoły</span>
-                        <i class="fa fa-angle-left pull-right"></i>
-                    </a>
-                    <ul class="treeview-menu">
-                        <li><a href="pages/charts/chartjs.html"><i class="fa fa-circle-o"></i> Stanu technicznego</a></li>
-                        <li><a href="pages/charts/morris.html"><i class="fa fa-circle-o"></i> Rozkompletowanie</a></li>
-                        <li><a href="pages/charts/flot.html"><i class="fa fa-circle-o"></i> Naklejki</a></li>
-                    </ul>
-                </li>
-                <li><a href="logout.php"><i class="fa fa-laptop"></i> <span>Oprogramowanie</span></a></li>
-                <li class="treeview">
-                    <a href="#">
-                        <i class="fa fa-truck"></i>
-                        <span>Magazyn</span>
-                        <i class="fa fa-angle-left pull-right"></i>
-                    </a>
-                    <ul class="treeview-menu">
-                        <li><a href="pages/charts/flot.html"><i class="fa fa-circle-o"></i> Do odebrania</a></li>
-                        <li><a href="pages/charts/chartjs.html"><i class="fa fa-circle-o"></i> 015A</a></li>
-                        <li><a href="pages/charts/morris.html"><i class="fa fa-circle-o"></i> Główny</a></li>
-                        <li><a href="pages/charts/flot.html"><i class="fa fa-circle-o"></i> Przyjęć</a></li>
-                        <li><a href="pages/charts/flot.html"><i class="fa fa-circle-o"></i> Wybrakowania</a></li>
-                    </ul>
-                </li>
-                <li class="treeview">
-                    <a href="#">
-                        <i class="fa fa-gears"></i>
-                        <span>Ewidencja</span>
-                        <i class="fa fa-angle-left pull-right"></i>
-                    </a>
-                    <ul class="treeview-menu">
-                        <li><a href="wartosci.php"><i class="fa fa-circle-o"></i> Wartości</a></li>
-                        <li><a href="jednostki.php"><i class="fa fa-circle-o"></i> Jednostki</a></li>
-                    </ul>
-                </li>
-                <li class="treeview">
-                    <a href="#">
-                        <i class="fa fa-pencil"></i>
-                        <span>Spis</span>
-                        <i class="fa fa-angle-left pull-right"></i>
-                    </a>
-                    <ul class="treeview-menu">
-                        <li><a href="pages/charts/flot.html"><i class="fa fa-circle-o"></i> Sprzęt KWP</a></li>
-                        <li><a href="pages/charts/chartjs.html"><i class="fa fa-circle-o"></i> Drukarki KWP</a></li>
-                        <li><a href="pages/charts/morris.html"><i class="fa fa-circle-o"></i> Wersja CPG</a></li>
-                        <li><a href="pages/charts/flot.html"><i class="fa fa-circle-o"></i> Wersja Firmware</a></li>
-                        <li><a href="pages/charts/flot.html"><i class="fa fa-circle-o"></i> Tonery</a></li>
-                        <li><a href="pages/charts/flot.html"><i class="fa fa-circle-o"></i> Weryfikacja</a></li>
-                        <li><a href="pages/charts/flot.html"><i class="fa fa-circle-o"></i> Interwencja</a></li>
-                    </ul>
-                </li>
-                <li class="treeview">
-                    <a href="#">
-                        <i class="fa fa-gears"></i>
-                        <span>Administracja</span>
-                        <i class="fa fa-angle-left pull-right"></i>
-                    </a>
-                    <ul class="treeview-menu">
-                        <li><a href="uzytkownicy.php"><i class="fa fa-circle-o"></i> Użytkownicy</a></li>
-                        <li><a href="uzytkownicy_grupa.php"><i class="fa fa-circle-o"></i>Grupy </a> </li>
-                        <li><a href="backup.php"><i class="fa fa-circle-o"></i> Backup</a></li>
-                        <li><a href="lista_zmian.php"><i class="fa fa-circle-o"></i> Lista zmian</a></li>
-                        <li><a href="pages/charts/flot.html"><i class="fa fa-circle-o"></i> Aktualizacja bazy</a></li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="pages/mailbox/mailbox.html">
-                        <i class="fa fa-envelope"></i> <span>Wiadomości</span>
-                        <?php
-                        $id_uzytkowanika2=$user_data['user_id'];
-                        $pobierzLiczbeWiadomosciSkrzynkaOdbiorcza_Nowe2=mysqli_query($polaczenie,"SELECT `id` FROM `wiadomosci` WHERE `id_odbiorcy`='$id_uzytkowanika2' AND `stan`=1") or die(mysqli_error($polaczenie));
-                        $LiczbeWiadomosciSkrzynkaOdbiorcza_Nowe2=mysqli_num_rows($pobierzLiczbeWiadomosciSkrzynkaOdbiorcza_Nowe2);
-                        if($LiczbeWiadomosciSkrzynkaOdbiorcza_Nowe2>0) {
-                            echo "<small class='label pull-right bg-yellow'>$LiczbeWiadomosciSkrzynkaOdbiorcza_Nowe2 </small>";
-                        }
-                        ?>
-                    </a>
-                </li>
-                <li><a href="nagroda.php"><i class="fa fa-money"></i> <span>Nagroda</span></a></li>
-                <li><a href="logout.php"><i class="fa fa-coffee"></i> <span>Wyloguj</span></a></li>
-            </ul>
-        </section>
-        <!-- /.sidebar -->
-    </aside>
+            </div>
+        </form>
+        <!-- /.search form -->
+        <!-- sidebar menu: : style can be found in sidebar.less -->
+        <ul class="sidebar-menu">
+            <li class="header">MAIN NAVIGATION</li>
+            <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-dashboard"></i> <span>Dashboard</span>
+                    <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+                </a>
+                <ul class="treeview-menu">
+                    <li><a href="./index.html"><i class="fa fa-circle-o"></i> Dashboard v1</a></li>
+                    <li><a href="./index2.html"><i class="fa fa-circle-o"></i> Dashboard v2</a></li>
+                </ul>
+            </li>
+            <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-files-o"></i>
+                    <span>Layout Options</span>
+                    <span class="pull-right-container">
+              <span class="label label-primary pull-right">4</span>
+            </span>
+                </a>
+                <ul class="treeview-menu">
+                    <li><a href="./layout/top-nav.html"><i class="fa fa-circle-o"></i> Top Navigation</a></li>
+                    <li><a href="./layout/boxed.html"><i class="fa fa-circle-o"></i> Boxed</a></li>
+                    <li><a href="./layout/fixed.html"><i class="fa fa-circle-o"></i> Fixed</a></li>
+                    <li><a href="./layout/collapsed-sidebar.html"><i class="fa fa-circle-o"></i> Collapsed Sidebar</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="./widgets.html">
+                    <i class="fa fa-th"></i> <span>Widgets</span>
+                    <span class="pull-right-container">
+              <small class="label pull-right bg-green">Hot</small>
+            </span>
+                </a>
+            </li>
+            <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-pie-chart"></i>
+                    <span>Charts</span>
+                    <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+                </a>
+                <ul class="treeview-menu">
+                    <li><a href="./charts/chartjs.html"><i class="fa fa-circle-o"></i> ChartJS</a></li>
+                    <li><a href="./charts/morris.html"><i class="fa fa-circle-o"></i> Morris</a></li>
+                    <li><a href="./charts/flot.html"><i class="fa fa-circle-o"></i> Flot</a></li>
+                    <li><a href="./charts/inline.html"><i class="fa fa-circle-o"></i> Inline charts</a></li>
+                </ul>
+            </li>
+            <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-laptop"></i>
+                    <span>UI Elements</span>
+                    <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+                </a>
+                <ul class="treeview-menu">
+                    <li><a href="./UI/general.html"><i class="fa fa-circle-o"></i> General</a></li>
+                    <li><a href="./UI/icons.html"><i class="fa fa-circle-o"></i> Icons</a></li>
+                    <li><a href="./UI/buttons.html"><i class="fa fa-circle-o"></i> Buttons</a></li>
+                    <li><a href="./UI/sliders.html"><i class="fa fa-circle-o"></i> Sliders</a></li>
+                    <li><a href="./UI/timeline.html"><i class="fa fa-circle-o"></i> Timeline</a></li>
+                    <li><a href="./UI/modals.html"><i class="fa fa-circle-o"></i> Modals</a></li>
+                </ul>
+            </li>
+            <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-edit"></i> <span>Forms</span>
+                    <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+                </a>
+                <ul class="treeview-menu">
+                    <li><a href="./forms/general.html"><i class="fa fa-circle-o"></i> General Elements</a></li>
+                    <li><a href="./forms/advanced.html"><i class="fa fa-circle-o"></i> Advanced Elements</a></li>
+                    <li><a href="./forms/editors.html"><i class="fa fa-circle-o"></i> Editors</a></li>
+                </ul>
+            </li>
+            <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-table"></i> <span>Tables</span>
+                    <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+                </a>
+                <ul class="treeview-menu">
+                    <li><a href="./tables/simple.html"><i class="fa fa-circle-o"></i> Simple tables</a></li>
+                    <li><a href="./tables/data.html"><i class="fa fa-circle-o"></i> Data tables</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="./calendar.html">
+                    <i class="fa fa-calendar"></i> <span>Calendar</span>
+                    <span class="pull-right-container">
+              <small class="label pull-right bg-red">3</small>
+              <small class="label pull-right bg-blue">17</small>
+            </span>
+                </a>
+            </li>
+            <li>
+                <a href="../mailbox/mailbox.html">
+                    <i class="fa fa-envelope"></i> <span>Mailbox</span>
+                    <span class="pull-right-container">
+              <small class="label pull-right bg-yellow">12</small>
+              <small class="label pull-right bg-green">16</small>
+              <small class="label pull-right bg-red">5</small>
+            </span>
+                </a>
+            </li>
+            <li class="treeview active">
+                <a href="#">
+                    <i class="fa fa-folder"></i> <span>Examples</span>
+                    <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+                </a>
+                <ul class="treeview-menu">
+                    <li><a href="invoice.html"><i class="fa fa-circle-o"></i> Invoice</a></li>
+                    <li><a href="profile.html"><i class="fa fa-circle-o"></i> Profile</a></li>
+                    <li><a href="login.html"><i class="fa fa-circle-o"></i> Login</a></li>
+                    <li><a href="register.html"><i class="fa fa-circle-o"></i> Register</a></li>
+                    <li><a href="lockscreen.html"><i class="fa fa-circle-o"></i> Lockscreen</a></li>
+                    <li><a href="404.html"><i class="fa fa-circle-o"></i> 404 Error</a></li>
+                    <li><a href="500.html"><i class="fa fa-circle-o"></i> 500 Error</a></li>
+                    <li class="active"><a href="blank.html"><i class="fa fa-circle-o"></i> Blank Page</a></li>
+                    <li><a href="pace.html"><i class="fa fa-circle-o"></i> Pace Page</a></li>
+                </ul>
+            </li>
+            <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-share"></i> <span>Multilevel</span>
+                    <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+                </a>
+                <ul class="treeview-menu">
+                    <li><a href="#"><i class="fa fa-circle-o"></i> Level One</a></li>
+                    <li>
+                        <a href="#"><i class="fa fa-circle-o"></i> Level One
+                            <span class="pull-right-container">
+                  <i class="fa fa-angle-left pull-right"></i>
+                </span>
+                        </a>
+                        <ul class="treeview-menu">
+                            <li><a href="#"><i class="fa fa-circle-o"></i> Level Two</a></li>
+                            <li>
+                                <a href="#"><i class="fa fa-circle-o"></i> Level Two
+                                    <span class="pull-right-container">
+                      <i class="fa fa-angle-left pull-right"></i>
+                    </span>
+                                </a>
+                                <ul class="treeview-menu">
+                                    <li><a href="#"><i class="fa fa-circle-o"></i> Level Three</a></li>
+                                    <li><a href="#"><i class="fa fa-circle-o"></i> Level Three</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                    <li><a href="#"><i class="fa fa-circle-o"></i> Level One</a></li>
+                </ul>
+            </li>
+            <li><a href="./documentation/index.html"><i class="fa fa-book"></i> <span>Documentation</span></a></li>
+            <li class="header">LABELS</li>
+            <li><a href="#"><i class="fa fa-circle-o text-red"></i> <span>Important</span></a></li>
+            <li><a href="#"><i class="fa fa-circle-o text-yellow"></i> <span>Warning</span></a></li>
+            <li><a href="#"><i class="fa fa-circle-o text-aqua"></i> <span>Information</span></a></li>
+        </ul>
+    </section>
+    <!-- /.sidebar -->
+</aside>
