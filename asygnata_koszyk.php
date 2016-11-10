447c2029ccf5a0e085eedc8655d3f3bd3912e721
mario <morawiec@vp.pl>
2016-11-10 15:06:50 +0100

    przebudowa karty środka trwałego dodanie modulu raportowania bledow wydzielenie inveo budowa funkcji generujacej naklejki budowa funkcji zarzadzajacej naklejkami budowa slownika budowa dodanie nowego srodka trwalego budowa modulu agend - koszyk budowa mechanizmu importu danych - konwektowanie bazy z s1 do s2 i zmiana jednostek budowa generatora agendy

--- asygnata_koszyk.php
+++ asygnata_koszyk.php
@@ -87,12 +87,24 @@ include 'menu.php';
                     $id_sprzetu = $_POST['id_sprzetu'];
                     $liczba_elementow = count($id_sprzetu);
 
+
                     echo "<table class='table table-bordered'><form method='post' action='asygnata_koszyk.php?a=zapisz'>";
                     //Glowne elementy
                     echo "<tr><th>Typ Asygnaty:</th><td><select class='form-control' name='typ_asygnaty'><option value='0'>Wydania</option><option value='1'>Przyjecia</option></select></td></tr>";
+                    if($liczba_elementow>0)
+                    {
+                        $czy_zgodne = SprawdzWydzialyAsygnata($id_sprzetu);
+                        if($czy_zgodne==false)
+                        {
+                            echo "<tr><th>Z stanu:</th><td><select name='wydzial_od' class='form-control'>";
+                            echo PobierzJednostki("");
+                            echo "</select> </td></tr>";
+                        }
+                    }
                     echo "<tr><th>Na stan:</th><td><select name='wydzial_do' class='form-control'>";
                     echo PobierzJednostki("");
                     echo "</select> </td></tr>";
+                    echo "<tr><th>Data dokumentu</th><td><input type='text' name='data_dokumentu' id='datepicker' value='$data_skrocona' class='form-control datepicker'></td></tr>";
                     echo "<tr><th>Uwagi:</th><td><textarea class='form-control' rows='6' name='uwagi'></textarea></td></tr>";
                     echo "<tr><th class='text-center' colspan='2'>Skladniki Asygnaty:</th></tr>";
                     //Elementy Asygnaty
@@ -109,7 +121,61 @@ include 'menu.php';
                 }
                 elseif ($a=='zapisz')
                 {
-                    var_dump($_POST);
+
+                    //Dane z post
+                    //Dane do głownej asygnaty
+                    $asygnata_typ = $_POST['typ_asygnaty'];
+                    $asygnata_nr = PobierzNrAsygnaty();
+                    AktualizujNrAsygnaty((int)$asygnata_nr);
+                    //format nr asygnaty
+                    $asygnata_numer =$asygnata_nr."/WŁiI/".date("y");
+                    $asygnata_data = $_POST['data_dokumentu'];
+                    $asygnata_do = $_POST['wydzial_do'];
+                    $asygnata_do_nazwa = PobierzNazweWydzialu($asygnata_do);
+                    $tablica_id_lp = $_POST['tablica_id'];
+
+                    $asygnata_uwagi = addslashes($_POST['uwagi']);
+                    $liczba_elementow_tablicy = count($tablica_id_lp);
+                    if($liczba_elementow_tablicy>1)
+                    {
+                        $asygnata_od =$_POST['wydzial_od'];
+                        $asygnata_od_nazwa = PobierzNazweWydzialu($asygnata_od);
+                    }
+                    else
+                    {
+                        $id_st = $_POST['tablica_id'];
+                        $asygnata_od = PobierzWydzialSprzetu($id_st[0]);
+                        $asygnata_od_nazwa = PobierzNazweWydzialu($asygnata_od);
+                    }
+
+                    //Zapisz glowna asygnate
+                    $zapisz_asygnate = mysqli_query($polaczenie,"INSERT INTO asygnata (nr_asygnaty, od, id_od, do, id_do, typ, kto_dodal, data_asygnaty, data_dodania, akceptacja, uwagi) 
+                    VALUES ('$asygnata_numer','$asygnata_od_nazwa','$asygnata_od','$asygnata_do_nazwa','$asygnata_do','$asygnata_typ','$użytkownik_imie_nazwisko','$asygnata_data','$data_pelana',0,'$asygnata_uwagi')")
+                    or die("Blad przy zapisz_asygnate".mysqli_error($polaczenie));
+                    $nr_id_asygnaty = mysqli_insert_id($polaczenie);
+                    //dane do skladników asygnaty
+                    $tablica_uwag = $_POST['uwaga_osobno'];
+                    for($ii = 0; $ii<$liczba_elementow_tablicy;$ii++)
+                    {
+                        $id_lp = $tablica_id_lp[$ii];
+                        $od = PobierzWydzialSprzetu($id_lp);
+                        $od = PobierzNazweWydzialu($od);
+
+                        $uwaga = addslashes($tablica_uwag[$ii]);
+
+                        $zapisz_skladnik_asygnaty = mysqli_query($polaczenie,"INSERT INTO asygnata_skladniki (id_lp, id_asygnaty, od, do, uwaga, nr_asygnaty) 
+                        VALUES ('$id_lp','$nr_id_asygnaty','$od','$asygnata_do_nazwa','$uwaga','$asygnata_numer')")
+                            or die("Blad przy zapisz_skladnik_asygnaty".mysqli_query($polaczenie));
+                    //usun pozyje z koszyka
+                        $usun_z_koszyka = mysqli_query($polaczenie,"DELETE FROM asygnata_koszyk WHERE id_lp = '$id_lp'") or die("Blad przy usun_z_koszyka".mysqli_error($polaczenie));
+                    }
+
+                    echo "Zapisano Pomyśnie asygnate $asygnata_numer. Liczba składników asygnaty $liczba_elementow_tablicy";
+
+
+
+
+
                 }
                 else
                 {
