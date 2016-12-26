6c32007711e1a39cc35f3cdfd041f2beefbd0a28
morawiecm <morawiec@vp.pl>
2016-12-26 18:48:01 +0100

    - poprawa znaków specjalnych w konstrukcji hasła - poprawa numeracji w protokołach

--- funkcje/funkcje_protokol_st.php
+++ funkcje/funkcje_protokol_st.php
@@ -12,7 +12,7 @@ function PobierzNrProtokoluST()
     $polaczenie = polaczenie_z_baza();
     //pobierz numer porotokolu z bazy
 
-    $pobierz_numer_protokolu = mysqli_query($polaczenie,"SELECT tresc FROM ustawienia WHERE id = '4'") or die("Blad przy pobierz_numer_protokolu".mysqli_error($polaczenie));
+    $pobierz_numer_protokolu = mysqli_query($polaczenie,"SELECT tresc FROM ustawienia WHERE id = '5'") or die("Blad przy pobierz_numer_protokolu".mysqli_error($polaczenie));
     while ($numer = mysqli_fetch_array($pobierz_numer_protokolu))
     {
         $nr_protokolu_st = $numer['tresc'];
@@ -25,6 +25,6 @@ function ZwiekszNrProtokoluST($nr_aktualny)
     $polaczenie = polaczenie_z_baza();
     //zwieksz numer porotokolu z bazy
 
-    $zwieksz_numer_protokolu = mysqli_query($polaczenie,"UPDATE ustawienia SET tresc = '$nr_aktualny' WHERE id = '4'") or die("Blad przy zwieksz_numer_protokolu".mysqli_error($polaczenie));
+    $zwieksz_numer_protokolu = mysqli_query($polaczenie,"UPDATE ustawienia SET tresc = '$nr_aktualny' WHERE id = '5'") or die("Blad przy zwieksz_numer_protokolu".mysqli_error($polaczenie));
 
 }
